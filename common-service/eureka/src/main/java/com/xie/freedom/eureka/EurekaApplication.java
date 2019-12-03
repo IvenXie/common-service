4d144cafd9f42fe60c19aaadd449748fe85cf86e
谢振东 <xiezhendong@fehorizon.com>
2019-12-03 16:30:58 +0800

    first commit

---
+++ common-service/eureka/src/main/java/com/xie/freedom/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.xie.freedom.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心服务
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
